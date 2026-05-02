2b59a467f8765863a7450451b45e56268fe6589e
Sagar6250 <sagars6250@gmail.com>
2026-05-02 02:42:56 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
     plugins: [react()],
     // IMPORTANT: Change 'portfolio' to your GitHub repo name
     // e.g. if your repo is github.com/sagar/my-portfolio, set base: '/my-portfolio/'
-    base: "/sagars6250.github.io/",
+    base: "/",
 });
